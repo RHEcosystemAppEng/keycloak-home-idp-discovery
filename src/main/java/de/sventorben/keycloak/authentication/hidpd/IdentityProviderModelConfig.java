03ff257ea40c6387d019323b94505f5649de8afa
Saurabh Agarwal <sauagarw@redhat.com>
2023-06-28 19:08:34 -0400

    Use mapper to specify domain mapping to IdP

--- src/main/java/de/sventorben/keycloak/authentication/hidpd/IdentityProviderModelConfig.java
+++ src/main/java/de/sventorben/keycloak/authentication/hidpd/IdentityProviderModelConfig.java
@@ -1,19 +1,26 @@
 package de.sventorben.keycloak.authentication.hidpd;
 
-import org.keycloak.models.Constants;
-import org.keycloak.models.IdentityProviderModel;
-
 import java.util.Arrays;
 import java.util.stream.Stream;
+
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.broker.provider.HardcodedAttributeMapper;
+import org.keycloak.models.Constants;
+import org.keycloak.models.IdentityProviderMapperModel;
+import org.keycloak.models.IdentityProviderModel;
+import org.jboss.logging.Logger;
 
 final class IdentityProviderModelConfig {
 
     private static final String DOMAINS_ATTRIBUTE_KEY = "home.idp.discovery.domains";
+    private static final Logger LOG = Logger.getLogger(IdentityProviderModelConfig.class);
 
     private final IdentityProviderModel identityProviderModel;
+    private final AuthenticationFlowContext context;
 
-    IdentityProviderModelConfig(IdentityProviderModel identityProviderModel) {
+    IdentityProviderModelConfig(AuthenticationFlowContext context, IdentityProviderModel identityProviderModel) {
         this.identityProviderModel = identityProviderModel;
+        this.context = context;
     }
 
     boolean hasDomain(String userAttributeName, String domain) {
@@ -23,6 +30,18 @@ final class IdentityProviderModelConfig {
     private Stream<String> getDomains(String userAttributeName) {
         String key = getDomainConfigKey(userAttributeName);
         String domainsAttribute = identityProviderModel.getConfig().getOrDefault(key, "");
+        if (domainsAttribute.length() == 0) {
+        	 Stream<IdentityProviderMapperModel> mappers = context.getRealm().getIdentityProviderMappersByAliasStream(identityProviderModel.getAlias());
+        	 IdentityProviderMapperModel mapper = mappers.filter(p -> DOMAINS_ATTRIBUTE_KEY.equals(p.getConfig().get(HardcodedAttributeMapper.ATTRIBUTE)))
+        	 		.findFirst()
+        	 		.orElse(null);
+        	 if (mapper != null) {
+        		 domainsAttribute = mapper.getConfig().getOrDefault(HardcodedAttributeMapper.ATTRIBUTE_VALUE, "");
+                 LOG.tracef("Found attribute mapper for '%s' with domain '%s'", DOMAINS_ATTRIBUTE_KEY, domainsAttribute);
+        	 } else {
+                 LOG.tracef("No attribute mapper found for '%s'", DOMAINS_ATTRIBUTE_KEY);
+             }
+        }
         return Arrays.stream(Constants.CFG_DELIMITER_PATTERN.split(domainsAttribute));
     }
 
--- src/test/java/de/sventorben/keycloak/authentication/hidpd/IdentityProviderModelConfigTest.java
+++ src/test/java/de/sventorben/keycloak/authentication/hidpd/IdentityProviderModelConfigTest.java
@@ -1,17 +1,43 @@
 package de.sventorben.keycloak.authentication.hidpd;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.keycloak.models.IdentityProviderModel;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.broker.provider.HardcodedAttributeMapper;
+import org.keycloak.models.IdentityProviderMapperModel;
+import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.RepresentationToModel;
+import org.keycloak.representations.idm.IdentityProviderMapperRepresentation;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class IdentityProviderModelConfigTest {
+	
+	@Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    AuthenticationFlowContext context;
 
     private static final String DOMAINS_ATTRIBUTE_KEY = "home.idp.discovery.domains";
+
+    @BeforeEach
+    void setUp() {
+        when(context.getRealm()).thenReturn(mock(RealmModel.class));
+        when(context.getUser()).thenReturn(mock(UserModel.class));
+    }
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -36,7 +62,7 @@ class IdentityProviderModelConfigTest {
         Map<String, String> config = new HashMap<>();
         IdentityProviderModel idp = new IdentityProviderModel();
         idp.setConfig(config);
-        IdentityProviderModelConfig cut = new IdentityProviderModelConfig(idp);
+        IdentityProviderModelConfig cut = new IdentityProviderModelConfig(context, idp);
         if (userAttributeName != null && userAttributeDomains != null) {
             config.put(DOMAINS_ATTRIBUTE_KEY + "." + userAttributeName, userAttributeDomains);
         }
@@ -48,5 +74,36 @@ class IdentityProviderModelConfigTest {
 
         assertThat(result).isEqualTo(expected);
     }
+    
+    @ParameterizedTest
+    @CsvSource(value = {
+    	"null, null, false",
+        "email, example.com##example.org, true",
+        "email, example.net##example.org, false",
+    }, nullValues = { "null" })
+    void testMapperHasDomain(String userAttributeNameQuery, String defaultDomains, boolean expected) {
+        
+    	Map<String, String> config = new HashMap<>();
+        IdentityProviderModel identityProviderModel = new IdentityProviderModel();
+        identityProviderModel.setAlias("keycloak-oidc");
+        identityProviderModel.setConfig(config);
+        
+        IdentityProviderMapperRepresentation rep = new IdentityProviderMapperRepresentation();
+        rep.setName("domain-mapper");
+        rep.setIdentityProviderAlias(identityProviderModel.getAlias());
+        rep.setIdentityProviderMapper("hardcoded-attribute-idp-mapper");
+        rep.getConfig().put(HardcodedAttributeMapper.ATTRIBUTE, DOMAINS_ATTRIBUTE_KEY);
+        rep.getConfig().put(HardcodedAttributeMapper.ATTRIBUTE_VALUE, defaultDomains);
+        
+        IdentityProviderMapperModel mapperModel = RepresentationToModel.toModel(rep);
+        IdentityProviderMapperModel[] mappers = {mapperModel} ;
+        
+        given(context.getRealm().getIdentityProviderMappersByAliasStream(mapperModel.getIdentityProviderAlias()))
+        .willReturn(Arrays.stream(mappers));
+        
+        IdentityProviderModelConfig cut = new IdentityProviderModelConfig(context, identityProviderModel);
+        boolean result = cut.hasDomain(userAttributeNameQuery, "example.com");
 
+        assertThat(result).isEqualTo(expected);
+    }
 }
